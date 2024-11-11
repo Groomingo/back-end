f9da9cea4c1d2c1f2605d76d8e647d71963774e9
wuchangb <kkang0820qus@gmail.com>
2024-11-11 21:07:21 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/groomingo/GroomingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.groomingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomingoApplication.class, args);
+	}
+
+}
